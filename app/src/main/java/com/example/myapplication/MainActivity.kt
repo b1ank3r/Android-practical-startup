6522b7786e492abea2d9d1780e9cb7ae2065398d
anreh <anurehka@gmail.com>
2023-02-22 11:50:53 +0800

    Commit added button, text for rolled dice and popup text for each time dice is rolled. 20230222

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //get the button view from the layout and assign a click
+        //listener to it
+        val rollButton: Button = findViewById(R.id.rolbtn)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    /**
+     * Click listener for the roll Button
+     */
+    private fun rollDice(){
+
+        Toast.makeText(this,"Dice is rolled", Toast.LENGTH_SHORT).show()
+
+        val randomInt =(1..6).random()
+
+        val resultText: TextView = findViewById(R.id.txtNumber)
+        resultText.text = randomInt.toString()
     }
 }
